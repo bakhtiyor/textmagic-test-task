ba9b448e5229303031a6b4c5becbc93b54f1f941
Bakhtiyor Bahritidinov <bakhtiyor.bahriddinov@gmail.com>
2024-04-29 22:41:29 +0500

    Create User entity

---
+++ migrations/Version20240429172311.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240429172311 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('
+            CREATE TABLE "user" (
+                id UUID NOT NULL, 
+                email VARCHAR(180) NOT NULL, 
+                roles JSON NOT NULL, 
+                password VARCHAR(255) NOT NULL, 
+                PRIMARY KEY(id)
+            )'
+        );
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)');
+        $this->addSql('COMMENT ON COLUMN "user".id IS \'(DC2Type:uuid)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE "user"');
+    }
+}
