c0c7cdf7db7312534f225d3d52d8ec5691ff2648
Bakhtiyor Bahritidinov <bakhtiyor.bahriddinov@gmail.com>
2024-05-03 02:07:16 +0500

    Fix psalm errors

--- src/Enum/UserQuizStatus.php
+++ src/Enum/UserQuizStatus.php
@@ -7,14 +7,4 @@ enum UserQuizStatus: string
     case QUEUED = 'queued';
     case STARTED = 'started';
     case FINISHED = 'finished';
-
-    public static function toArray(): array
-    {
-        $values = [];
-        foreach (self::cases() as $enum) {
-            $values[$enum->name] = $enum->value;
-        }
-
-        return $values;
-    }
 }
